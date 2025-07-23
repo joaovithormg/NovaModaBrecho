6e0610fc1ca48c5fdba85585ae0796e6be8b4be9
Maria Eduarda Contu <mariaescontu@gmail.com>
2025-07-23 15:36:24 -0300

    ✨  feat: SellingPrice method implemented.

--- NovaModaBrecho/Services/ItemService.cs
+++ NovaModaBrecho/Services/ItemService.cs
@@ -1,6 +1,22 @@
+using NovaModaBrecho.Models;
+using NovaModaBrecho.Models.Enums;
+using NovaModaBrecho.Services.Interfaces;
+
 namespace NovaModaBrecho.Services;
 
-public class ItemService
+public class ItemService : IItemService
 {
-    
+    public double SellingPrice(Item item)
+    {
+        switch (item.Condition)
+        {
+            case Condition.New:
+                return item.OriginalPrice * 0.7;
+            case Condition.Good:
+                return item.OriginalPrice * 0.6;
+            default: //case fair
+                return item.OriginalPrice * 0.5;
+        }
+        return 0;
+    }
 }
