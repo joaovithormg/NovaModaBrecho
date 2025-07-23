96ea638c5c61df35a26c30e6ffb8de585e5cff25
Joao Vithor Moraes Goncalves <JoaoVithor.Goncalves@dellteam.com>
2025-07-23 13:50:46 -0300

    :tada: first commit

---
+++ NovaModaBrecho/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace NovaModaBrecho.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
